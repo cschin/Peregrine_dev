7e9b8972c835bbfc546f935bb4b6d23beb838ca2
Jason Chin <cschin@infoecho.net>
2020-01-05 01:28:30 -0800

    add ovlp_to_graph_aggressive.py

--- py/setup_pypy.py
+++ py/setup_pypy.py
@@ -5,4 +5,6 @@ os.environ["peregrine_base"] = os.path.abspath(os.path.pardir)
 setup(name='peregrine_pypy',
       version='0.1',
       install_requires=["networkx>=2.4"],
-      scripts = ["scripts/ovlp_to_graph.py", "scripts/graph_to_path.py"])
+      scripts = ["scripts/ovlp_to_graph.py",
+                 "scripts/ovlp_to_graph_aggressive.py",
+                 "scripts/graph_to_path.py"])
