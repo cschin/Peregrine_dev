f8762e6d1f02849824a753cdbed1924df3d3ae7b
Jason Chin <cschin@infoecho.net>
2020-03-14 20:26:15 +0000

    only remove utg < 50000bp for extra utgs

--- py/in_dev/get_a_ctg.py
+++ py/in_dev/get_a_ctg.py
@@ -17,16 +17,26 @@ ctg_sdb=SequenceDatabase("pa_phase_ctg.idx", "pa_phase_ctg.seqdb")
 
 htig_paths = {}
 ctgs = set()
+
+p_ctg_file = open("p_utg_test.fa", "w")
+a_ctg_file = open("a_utg_test.fa", "w")
+extra_ctg_file = open("extra_utg_test.fa", "w")
 with open("ctg_assign.txt") as f:
     for r in f:
         r = r.strip().split()
         p_ctg, a_ctg, t = r
-        if int(t) == 0:
-            continue
         seq = ctg_sdb.get_subseq_by_name(a_ctg)
-        if len(seq) < 50000:
-            continue
-        print(f">{p_ctg}/{a_ctg}")
-        print(seq.decode())
-
-
+        if int(t) == 0:
+            print(f">{p_ctg}/{a_ctg}", file=p_ctg_file)
+            print(seq.decode(), file=p_ctg_file)
+        if int(t) == 1:
+            print(f">{p_ctg}/{a_ctg}", file=a_ctg_file)
+            print(seq.decode(), file=a_ctg_file)
+        elif int(t) == 2:
+            if len(seq) < 50000:
+                continue
+            print(f">{p_ctg}/{a_ctg}", file=extra_ctg_file)
+            print(seq.decode(), file=extra_ctg_file)
+p_ctg_file.close()
+a_ctg_file.close()
+extra_ctg_file.close() 
