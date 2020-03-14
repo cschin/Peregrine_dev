21994007f3875dfe5a4ee0269c4a297b10fe438d
Jason Chin <cschin@infoecho.net>
2020-03-14 20:24:13 +0000

    Modify the merging algorithm

--- py/in_dev/merge_phased_utg.py
+++ py/in_dev/merge_phased_utg.py
@@ -16,13 +16,20 @@ from multiprocessing import Pool
 
 def get_ctg2match(map_fn, ctg_sdb):
     ctg2match = {}
+    rctg2path = {}
     with open(map_fn) as f:
         for row in f:
             row = [int(c) for c in row.strip().split()]
             ctg_name = ctg_sdb.index_data[row[3]].rname
             ctg2match.setdefault(ctg_name,[])
             ctg2match[ctg_name].append(row)
-    return ctg2match
+            
+            rctg_name = ref_sdb.index_data[row[0]].rname 
+            rctg2path.setdefault(rctg_name, {})
+            k = (row[1], row[2])
+            rctg2path[rctg_name].setdefault(k,[])
+            rctg2path[rctg_name][k].append(row) 
+    return ctg2match, rctg2path
 
 
 def get_best_match(ctg2match):
@@ -63,27 +70,16 @@ def get_match_group(matches):
         groups.append(group)
     return groups
 
-
-def get_marker_link(rctg, ref_sdb, ctg2match, r_ctg2ctg, excluded=set()):
-    markers = set()
-    links = {}
-    s0=ref_sdb.get_subseq_by_name(rctg)
-    if rctg not in r_ctg2ctg:
-        markers = (0, ref_sdb.get_seq_index_by_name(rctg).length-32)
-        return markers, links
-
+def get_ctg2group(r_ctg2ctg, rctg):
+    ctg2groups = {}
     for ctg, direction in r_ctg2ctg[rctg]:
-        if ctg in excluded:
-            continue
+        #print(ctg)
         r_ctg_id = ref_sdb.name2rid[rctg]
         matches = []
         for r in ctg2match[ctg]:
             if r[0] != r_ctg_id or r[6] != direction:
                 continue
-            #if r[-1] > 10:
-            #    continue
             matches.append(r)
-        # print(ctg, direction, len(matches))
         m_group = get_match_group(matches)
         for mg in m_group:
             if len(mg) < 2:
@@ -95,132 +91,115 @@ def get_marker_link(rctg, ref_sdb, ctg2match, r_ctg2ctg, excluded=set()):
             t1 = mg[-1][5]
             d1 = mg[0][6]
             d2 = mg[-1][6]
-            assert d1 == direction
-            assert d2 == direction
-            links[(s0, t0)] = ctg, s1, t1, direction
-            markers.add(s0)
-            markers.add(t0)
-    if len(markers) < 2:
-        links = {}
-        markers = (0, ref_sdb.get_seq_index_by_name(rctg).length-32)
-        return markers, links
+            ctg2groups.setdefault(ctg, [])
+            ctg2groups[ctg].append( (s0, t0, s1, t1, direction) )
+    return ctg2groups
 
-    markers = list(markers)
-    markers.sort()
-    return markers, links
-
-def get_filtered_links(rctg, links):
-    intervals = []
-    filtered_links = {}
-    filtered_markers = set()
-    
-    keys = list(links.keys())
-    keys.sort(key=lambda x:x[0]-x[1])
-
-    for s, t in keys:
-        flag = 0
-        for itvl in intervals:
-            if s > itvl[0] and s < itvl[1]:
-                flag = 1
-                continue
-            if t > itvl[0] and t < itvl[1]:
-                flag = 1
-                continue
-        if flag == 1:
+def get_layout_segements(r_ctg2ctg, rctg, ctg_sdb, ctg2groups, phase="H1", exclude_ctgs = set()):
+    used_ctgs = set() 
+    segs = []
+    seg_end = -1
+    bb_bgn = -1
+    bb_end = -1 
+    for k in sorted(rctg2path[rctg].keys()):
+        if k[0] < seg_end:
             continue
-        filtered_links[(s,t)] = links[(s,t)]
-        intervals.append((s, t))
-        filtered_markers.add(s)
-        filtered_markers.add(t)
-    filtered_markers = list(filtered_markers)
-    filtered_markers.sort()
-    if len(filtered_markers) < 2:
-        links = {}
-        markers = (0, ref_sdb.get_seq_index_by_name(rctg).length-32)
-        return markers, links
-    return filtered_markers, filtered_links
+        
+        candidates = []
+
+        for m in rctg2path[rctg][k]:
+            ctg = ctg_sdb.index_data[m[3]].rname
+            if ctg not in ctg2groups:
+                continue
+            if ctg in exclude_ctgs:
+                continue
+            for g in ctg2groups[ctg]:
+                candidates.append((g, ctg))
+        
+        if len(candidates) != 0:
+            candidates2 = []
+            for c in candidates:
+                overlap = c[0][0] - seg_end
+                extend = c[0][1] - seg_end
+                if overlap > 10000:
+                    continue
+                    
+                # if extend > 2*abs(overlap) and overlap > -50000:
+                if extend > 2*abs(overlap):
+                    candidates2.append(c)
+
+            if len(candidates2) > 0:
+                candidates2.sort(key=lambda x: x[0][1])
+                c = candidates2[-1]
+                overlap = c[0][0] - seg_end
+                extend = c[0][1] - seg_end
+                s0, t0, s1, t1, direction = c[0]
+                ctg = c[1]
+                for m in rctg2path[rctg][k]:
+                    if ctg_sdb.index_data[m[3]].rname == c[1] and \
+                       m[1] == k[0]:
+                        s0 = m[1]
+                        s1 = m[4]
+
+                if bb_bgn == -1:
+                    bb_bgn = t0
+                else:
+                    segs.append( (f"{rctg}-{phase}", f"{rctg}", bb_bgn, s0, s0-bb_bgn, "backbone", bb_bgn, s0, 0) )
+                    bb_bgn = t0
+                segs.append( (f"{rctg}-{phase}", f"{rctg}", s0, t0, t0-s0, f"{ctg}", s1, t1, direction) )
+                used_ctgs.add(ctg)
+                seg_end = c[0][1]
+                
+            else:
+                if seg_end != k[1]:
+                    seg_end = k[1]
+    return segs, used_ctgs
 
 
 ref_sdb=SequenceDatabase("p_ctg_cns.idx", "p_ctg_cns.seqdb")
 ctg_sdb=SequenceDatabase("pa_phase_ctg.idx", "pa_phase_ctg.seqdb")
 
-ctg2match = get_ctg2match("p_ctg_cns-pa_phase_ctg-map", ctg_sdb)
+ctg2match, rctg2path = get_ctg2match("p_ctg_cns-pa_phase_ctg-map", ctg_sdb)
 ctg2best, r_ctg2ctg = get_best_match(ctg2match)
 
-
-def get_paths(rctg_id):
-    rtn = []
-    rctg_name = ref_sdb.index_data[rctg_id].rname
-    markers, links = get_marker_link(rctg_name, ref_sdb, ctg2match, r_ctg2ctg)
-    markers, links = get_filtered_links(rctg_name, links)
-
-    G=nx.DiGraph()
-    nx.add_path(G, markers, ctg="backbone")
-    for s, t in links:
-        ctg=links[(s,t)][0]
-        G.add_edge(s, t, ctg=ctg)
-    #print(len(G))
-    #nx.write_gexf(G,"test2.gexf")
-
-    #if len(markers) < 2:
-    #    print("XX", rctg_id, markers)
-    path=nx.shortest_path(G, markers[0], markers[-1])
-    edges = zip(path[0:-1], path[1:])
-    main_ctgs = set()
-    for s, t in edges:
-        if (s, t) in links:
-            ctg, s2, t2, d = links[(s,t)]
-            rtn.append((f"{rctg_name}-H1", rctg_name, s, t, t-s, ctg, s2, t2, d))
-            main_ctgs.add(ctg)
-        else:
-            rtn.append((f"{rctg_name}-H1", rctg_name, s, t, t-s, G[s][t]["ctg"], s, t, 0))
-
-    markers, links = get_marker_link(rctg_name, ref_sdb, ctg2match, r_ctg2ctg, excluded=main_ctgs)
-    markers, links = get_filtered_links(rctg_name, links)
-
-    G=nx.DiGraph()
-    nx.add_path(G, markers, ctg="backbone")
-    for s, t in links:
-        ctg=links[(s,t)][0]
-        G.add_edge(s, t, ctg=ctg)
-    #print(len(G))
-    #nx.write_gexf(G,"test2.gexf")
-
-    #if len(markers) < 2:
-    #    print("XX", rctg_id, markers)
-    path=nx.shortest_path(G, markers[0], markers[-1])
-    edges = zip(path[0:-1], path[1:])
-    secondary_ctgs = set()
-    for s, t in edges:
-        if (s, t) in links:
-            ctg, s2, t2, d = links[(s,t)]
-            rtn.append((f"{rctg_name}-H2", rctg_name, s, t, t-s, ctg, s2, t2, d))
-            secondary_ctgs.add(ctg)
-        else:
-            rtn.append((f"{rctg_name}-H2", rctg_name, s, t, t-s, G[s][t]["ctg"], s, t, 0))
-    additional_ctgs = set() 
-    if rctg_name  in r_ctg2ctg:
-        for ctg, _ in r_ctg2ctg[rctg_name]:
-            if ctg not in main_ctgs and ctg not in secondary_ctgs:
-                additional_ctgs.add(ctg)
-    return rtn, rctg_name, main_ctgs, secondary_ctgs, additional_ctgs
-
-p = Pool(20)
+all_used_ctgs = set()
 f = open("ctg_assign.txt", "w")
-for rtn, rctg_name, main_ctgs, secondary_ctgs, additional_ctgs in p.imap(get_paths, ref_sdb.index_data.keys()):
-    for l in rtn:
-        print(" ".join([str(_) for _ in l]))
-    all_ctgs = main_ctgs | secondary_ctgs | additional_ctgs
-    for ctg in list(all_ctgs):
-        if ctg in main_ctgs:
-            print( rctg_name, ctg, 0, file = f)
-        elif ctg in secondary_ctgs:
-            print( rctg_name, ctg, 1, file = f)
-        elif ctg in additional_ctgs: 
-            print( rctg_name, ctg, 2, file = f)
+for rctg in rctg2path:
+
+    if rctg not in r_ctg2ctg:
+        s0 = 0
+        t0 = ref_sdb.get_seq_index_by_name(rctg).length
+        segs = ((f"{rctg}-H1", f"{rctg}", s0, t0, t0-s0, "backbone", s0, t0, 0),)
+    else:
+        ctg2groups = get_ctg2group(r_ctg2ctg, rctg)
+        segs, p_utgs = get_layout_segements(r_ctg2ctg, rctg, ctg_sdb,
+                                               ctg2groups, phase="H1") 
+
+    for seg in segs:
+        print( " ".join([str(_) for _ in seg]) )
+
+    if rctg not in r_ctg2ctg:
+        s0 = 0
+        t0 = ref_sdb.get_seq_index_by_name(rctg).length
+        segs = ((f"{rctg}-H2", f"{rctg}", s0, t0, t0-s0, "backbone", s0, t0, 0),)
+    else:
+        ctg2groups = get_ctg2group(r_ctg2ctg, rctg)
+        segs, a_utgs = get_layout_segements(r_ctg2ctg, rctg, ctg_sdb,
+                                               ctg2groups, phase="H2", exclude_ctgs=p_utgs) 
+
+    for seg in segs:
+        print( " ".join([str(_) for _ in seg]) )
+
+    for n in p_utgs:
+        print(rctg, n, 0, file=f)
+
+    for n in a_utgs:
+        print(rctg, n, 1, file=f)
+    
+    if rctg in r_ctg2ctg:
+        s = set([_[0] for _ in r_ctg2ctg[rctg]])
+        for n in s - p_utgs - a_utgs:
+            print(rctg, n, 2, file=f)
+
+
 f.close()
-
-#for rtn in map(get_paths, ref_sdb.index_data.keys()):
-#    for l in rtn:
-#        print(" ".join([str(_) for _ in l]))
-
