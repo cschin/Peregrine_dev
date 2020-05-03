44cfa0351c3eb8f0762da51cdb580b5bc33c715f
Jason Chin <cschin@infoecho.net>
2020-05-03 21:18:17 +0000

    use single file as input

--- py/scripts/filter_ovlp.py
+++ py/scripts/filter_ovlp.py
@@ -5,20 +5,20 @@ with open("filtered_out_reads") as f:
         r = r.strip()
         #reads.add(r)
 phasable_reads = set()
-for fn in glob.glob("preads-ann.ovl.*"):
-    with open(fn) as f:
-        for r in f:
-            r = r.strip().split()
-            if r[0] in reads:
-                continue
-            if r[1] in reads:
-                continue
-            d = int(r[13])
-            n = int(r[14])
-            if d ==0 or 1.0*n/d > 0.96:
-                print(" ".join(r))
-                phasable_reads.add(r[0])
-                phasable_reads.add(r[1])
+fn = "preads-ann.ovl":
+with open(fn) as f:
+    for r in f:
+        r = r.strip().split()
+        if r[0] in reads:
+            continue
+        if r[1] in reads:
+            continue
+        d = int(r[13])
+        n = int(r[14])
+        if d ==0 or 1.0*n/d > 0.96:
+            print(" ".join(r))
+            phasable_reads.add(r[0])
+            phasable_reads.add(r[1])
 
 with open("phasable_reads", "w") as f:
     for rid in phasable_reads:
@@ -26,21 +26,21 @@ with open("phasable_reads", "w") as f:
 
 unphased_reads = set()
 
-for fn in glob.glob("preads-ann.ovl"):
-    with open(fn) as f:
-        for r in f:
-            r = r.strip().split()
-            if r[0] in reads:
-                continue
-            if r[1] in reads:
-                continue
-            if r[0] in phasable_reads:
-                continue
-            if r[1] in phasable_reads:
-                continue
-            unphased_reads.add(r[0])
-            unphased_reads.add(r[1])
-            #print(" ".join(r))
+fn ="preads-ann.ovl":
+with open(fn) as f:
+    for r in f:
+        r = r.strip().split()
+        if r[0] in reads:
+            continue
+        if r[1] in reads:
+            continue
+        if r[0] in phasable_reads:
+            continue
+        if r[1] in phasable_reads:
+            continue
+        unphased_reads.add(r[0])
+        unphased_reads.add(r[1])
+        #print(" ".join(r))
 print("-")
 with open("unphased_reads", "w") as f:
     for rid in unphased_reads:
