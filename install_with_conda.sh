01e90ccd2027a978770cdad54492c4d013540b56
Jason Chin <cschin@infoecho.net>
2020-06-14 19:13:49 +0000

    fix build/test

--- install_with_conda.sh
+++ install_with_conda.sh
@@ -22,11 +22,7 @@ conda create -n peregrine -y python=3.7
 
 conda activate peregrine
 
-pushd py
-rm -rf .eggs/ dist/ build/ peregrine.egg-info/ peregrine_pypy.egg-info get-pip.py
-python3 setup.py install
-python3 setup.py clean --all
-popd
+
 git clone -b peregrine https://github.com/cschin/pypeFLOW.git
 pushd pypeFLOW
 python3 setup.py install
@@ -35,9 +31,11 @@ popd
 conda install -c conda-forge -y pypy3.6
 conda install -c bioconda -y pysam ncls
 pushd py
+rm -rf .eggs/ dist/ build/ peregrine.egg-info/ peregrine_pypy.egg-info get-pip.py
 wget -q https://bootstrap.pypa.io/get-pip.py
 pypy3 get-pip.py
 pypy3 setup_pypy.py install
+python3 setup.py install
 popd
 
 pushd src
